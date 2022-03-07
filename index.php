7c81cefd7d38dd7a06645eefd978709dbc2afc17
Arnold Guevarra <j.guevarra@energin.co>
2022-03-07 17:00:21 +1100

    setup

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package Nevs Notifications
+ * @version 1.0.0
+ */
+
+/*
+Plugin Name: Nevs Notifications
+Plugin URI: https://github.com/jmguevarra/nevs-notifications
+Description: Simple plugin that notify certain users for newly created page/post
+Version: 1.0.0
+Author: Jaime I. Guevarra Jr.
+Author URI: https://github.com/jmguevarra/
+License: GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: nevs-notifications
+*/
+
+if(  !defined('WPINC') ){
+    die;
+}
+
+//define variables
+define('PLUGIN_DIR', dirname(__FILE__));
+define('PLUGIN_VERSION', '1.0.0');
+define('TEXT_DOMAIN', 'nevs-notifications');
+
+
+//require files to load
+//require_once 'includes/metabox.php';
+//require_once 'includes/enqueue-scripts.php';
+
+
+ 
